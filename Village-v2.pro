5eaccf615f8c82ec6a299f18fbeb7f3c566b1d82
Ferté Aymeric <aymeric.ferte@ynov.com>
2024-10-24 15:15:01 +0200

    adding files

--- Village-v2.pro
+++ Village-v2.pro
@@ -4,4 +4,12 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
+        auberge.cpp \
+        building.cpp \
+        character.cpp \
         main.cpp
+
+HEADERS += \
+    auberge.h \
+    building.h \
+    character.h
---
+++ character.h
@@ -0,0 +1,29 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+#include <string>
+
+using namespace std;
+
+class Character
+{
+    string m_name;
+    int m_attack;
+    int m_health;
+    int m_gold;
+    int m_defense;
+public:
+    Character();
+
+
+    void SetName(string name);
+
+    void SetAttack(int attack);
+
+    void SetHealth(int health);
+
+    void SetGold(int Gold);
+
+    void SetDefense(int Defense);
+};
+
+#endif // CHARACTER_H
